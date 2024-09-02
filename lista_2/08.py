bd81f4336de09e1742fab51c1389c1c341f205ed
Tamires <tamires.ei04@gmail.com>
2024-09-02 11:05:37 -0300

    Update 08.py

--- lista_2/08.py
+++ lista_2/08.py
@@ -1,6 +1,6 @@
 #Use um loop for e range para somar todos os números ímpares de 1 a 300 e exiba a soma
 
-num_impar = range(1, 300, 2)
-soma = sum(num_impar)
-
-print("A soma de todos os números ímpares entre 1 e 300 é {}".format(soma))
+soma = 0
+for num_impar in range(1, 301, 2):
+   soma+=num_impar
+print(soma)
