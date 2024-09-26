5ab4570300e8100770510968404eb886548b94f7
Tamires <tamires.ei04@gmail.com>
2024-09-26 09:30:07 -0300

    ainda tentando o desafio da mega sena

--- DESAFIO MEGA SENA/sorteio.py
+++ DESAFIO MEGA SENA/sorteio.py
@@ -1,12 +1,20 @@
 import random
+import csv
 
 lista = []
 contador = 0
 
 while contador < 6:
     num = random.randint(1,61)
-    lista.append(num)
     if num not in lista:
+        lista.append(num)
         contador+=1
+    else:
         pass
+
 print(lista)
+
+with open('C:\Users\202310164\Desktop\python\tarefas\DESAFIO MEGA SENA.csv', 'r') as csvfile:
+    leitor_csv = csv.reader(csvfile)
+    for linha in leitor_csv:
+        if all(lista in linha for lista in leitor)
